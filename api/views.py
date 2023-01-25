f210d786f547072ca2d81d6030149e04240de4f9
Ankit Pakhale <akp3067@gmail.com>
2023-01-25 15:11:50 +0530

    Added: put api

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from .models import Note
 from .serializers import NoteSerializer
 # Create your views here.
 
+
 @api_view(["GET", "POST"])
 def getRoutes(request):
     routes = [
@@ -41,14 +42,26 @@ def getRoutes(request):
     ]
     return Response(routes)
 
+
 @api_view(["GET"])
 def getNotes(request):
     notes = Note.objects.all()
     serializer = NoteSerializer(notes, many=True)
-    return Response(serializer.data) 
+    return Response(serializer.data)
+
 
 @api_view(["GET", "POST"])
 def getNote(request, pk):
     notes = Note.objects.get(id=pk)
     serializer = NoteSerializer(notes)
-    return Response(serializer.data) 
+    return Response(serializer.data)
+
+
+@api_view(["PUT"])
+def updateNote(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance=note, data=data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
